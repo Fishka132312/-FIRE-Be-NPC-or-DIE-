905833e09e589ba6ea00d84e81b1ab2a6344f7f9
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2025-01-10 23:34:34 +0100

    Update Candy.lua

--- Candy.lua
+++ Candy.lua
@@ -1,4 +1,23 @@
-while true do
-  					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.CollectableItems.Peppermint.CFrame
-wait(0.5)
+local player = game.Players.LocalPlayer
+
+-- Функция для обновления персонажа
+local function updateCharacter()
+    while true do
+        if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+            player.Character.HumanoidRootPart.CFrame = game.Workspace.CollectableItems.Peppermint.CFrame
+        end
+        wait(0.5)
+    end
+end
+
+-- Обработчик на смену персонажа (если персонаж умирает или респаунится)
+player.CharacterAdded:Connect(function()
+    -- Ждем немного, чтобы персонаж точно появился
+    wait(1)
+    updateCharacter()
+end)
+
+-- Если персонаж уже существует, запускаем скрипт сразу
+if player.Character then
+    updateCharacter()
 end
