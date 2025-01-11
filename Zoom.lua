a7ed1798c7b7da9b38f570bc433124cfadd4cb93
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2025-01-11 16:55:49 +0100

    Delete Zoom.lua

--- Zoom.lua
+++
@@ -1,9 +0,0 @@
-while task.wait() do
-    local UserInputService = game:GetService("UserInputService")
-    local CHeld = UserInputService:IsKeyDown(Enum.KeyCode.C)
-    if CHeld == true then
-        game.workspace.CurrentCamera.FieldOfView = 1
-    else
-        game.workspace.CurrentCamera.FieldOfView = 111111
-    end
-end
