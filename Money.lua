484e157022d1677e181fa84ba540024e9a121049
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2025-01-11 20:09:41 +0100

    Update Money.lua

--- Money.lua
+++ Money.lua
@@ -1,18 +1,18 @@
 local player = game.Players.LocalPlayer
 
 local function teleportToCash()
-    while true do
-        local cash = game.Workspace.CollectableItems:FindFirstChild("Cash")
-        
-        if cash then
-            -- Используем CFrame, чтобы точно телепортировать персонажа к объекту
-            player.Character.HumanoidRootPart.CFrame = cash.CFrame
+    while wait(1) do
+        if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+            local cash = game.Workspace.CollectableItems:FindFirstChild("Cash")
             
-            -- Ждем некоторое время, чтобы не телепортировать слишком быстро
-            wait(0.5)
+            if cash then
+                player.Character.HumanoidRootPart.CFrame = cash.CFrame
+                
+                wait(0.5)
+            end
         else
-            -- Если объект "Cash" не найден, ждем немного и пробуем снова
-            wait(1)
+            warn("Персонаж недоступен, ожидаем...")
+            wait(2)
         end
     end
 end
