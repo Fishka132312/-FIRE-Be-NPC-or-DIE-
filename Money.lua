f709c44291dbd8fcb1d8018a8dd95079a30acd02
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2025-01-11 15:55:22 +0100

    Update Money.lua

--- Money.lua
+++ Money.lua
@@ -1,20 +1,16 @@
 local player = game.Players.LocalPlayer
 
--- Функция для телепортации персонажа к Peppermint
 local function teleportToCash()
     while true do
-        -- Проверяем, существует ли объект Peppermint в коллекции CollectableItems
         local cash = game.Workspace.CollectableItems:FindFirstChild("Cash")
         
-        -- Если объект найден, телепортируем игрока
         if cash then
-            wait(0.5)  -- Ждем 1 секунду перед телепортацией
-            player.Character.HumanoidRootPart.CFrame = cash.CFrame  -- Телепортируем персонажа
+            wait(0.5)
+            player.Character.HumanoidRootPart.CFrame = cash.CFrame
         end
         
-        wait(1)  -- Пауза между проверками, чтобы не перегружать процессор
+        wait(0.5)
     end
 end
 
--- Запускаем функцию для телепортации в цикле
 teleportToCash()
