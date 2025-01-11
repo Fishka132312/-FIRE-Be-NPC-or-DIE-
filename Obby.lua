09c262ccedf9d0bc80555808705918de94bbbe49
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2025-01-11 16:24:27 +0100

    Update Obby.lua

---
+++ Obby.lua
@@ -0,0 +1,4 @@
+while true do
+          game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-296.787506, 9.15189934, -713.220703, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+  wait(300)
+end
